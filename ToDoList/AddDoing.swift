4e2816b2be5bc42c4fbdcadadb4fa2b8400e9e93
eunsung <dmsqls6789@naver.com>
2023-03-10 02:40:27 +0900

    객체배열 만들어서 여러 날짜에 배열로 doing저장하기

--- ToDoList/AddDoing.swift
+++ ToDoList/AddDoing.swift
@@ -1,6 +1,10 @@
 import UIKit
 
 class AddDoing : UIViewController {
+    var doing:[Day] = []
+    var stringDate: String = ""
+    var dateKey: String = ""
+    
     let ud = UserDefaults.standard
     var arr:[String] = []
     
@@ -10,14 +14,54 @@ class AddDoing : UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         
-        if let DoingList = ud.stringArray(forKey: "arr") {
-            var List: String = ""
-            for i in 0..<DoingList.count {
-                List += (DoingList[i] + "\n")
-            }
-            lblList.text = List
-        }
+//        if let DoingList = ud.stringArray(forKey: "arr") {
+//            var List: String = ""
+//            for i in 0..<DoingList.count {
+//                List += (DoingList[i] + "\n")
+//            }
+//            lblList.text = List
+//        }
         
+        outputDoing()
+        
+    }
+    func test() {
+        //var test:String?
+        var test1:String = ""
+        //test = String(doing.count)
+//        if let tst = test {
+//            lblList.text = tst
+//        }
+        for i in 0..<doing.count {
+            test1 += doing[i].key!
+        }
+        lblList.text = test1
+    }
+    
+    func checkKey() -> Int {
+        for i in 0..<doing.count {
+            if(doing[i].key == dateKey) {
+                return i
+            }
+        }
+        return -1
+    }
+    
+    func getDoing() -> [Day] {
+        guard let savedData = ud.value(forKey: "day") as? Data,
+                    let loadDoing = try? PropertyListDecoder().decode([Day].self, from: savedData) else { return [] }
+                    return loadDoing
+    }
+    
+    func outputDoing() {
+        doing = getDoing()
+        
+        if(checkKey() != -1) {
+            lblList.text = doing[checkKey()].outputDoing()
+        } else {
+            lblList.text = ""
+        }
+        //test()
     }
     
     @IBAction func DatePicker(_ sender: UIDatePicker) {
@@ -26,21 +70,45 @@ class AddDoing : UIViewController {
         let dateFormat = DateFormatter()
         dateFormat.dateFormat = "MM월 dd일"
         
-        let StringDate = dateFormat.string(from: DatePick.date)
-        ud.set(StringDate, forKey: "DatePick")
+        stringDate = dateFormat.string(from: DatePick.date)
         
+        dateFormat.dateFormat = "yyyyMMdd"
+        dateKey = dateFormat.string(from: DatePick.date)
+        
+        //ud.set(stringDate, forKey: "DatePick")
+        outputDoing()
     }
     
     
     @IBAction func onAdd(_ sender: Any) {
-        if let arr = ud.stringArray(forKey: "arr") {
-            self.arr = arr
+//        if let arr = ud.stringArray(forKey: "arr") {
+//            self.arr = arr
+//        }
+//        arr.append(txtDoing.text!)
+//
+//        ud.set(arr, forKey: "arr")
+//
+//        viewWillAppear(true)
+        
+//        for i in 0..<doing.count {
+//            if (checkKey(i: i) == true) {
+//                    doing[i].addDoing(Doing: txtDoing.text!)
+//            } else if (i == doing.count-1) {
+//                doing.append(Day(key: dateKey, date: stringDate))
+//                doing[i].addDoing(Doing: txtDoing.text!)
+//            }
+//        }
+        let i = checkKey()
+        if (i != -1) {
+            doing[i].addDoing(Doing: txtDoing.text!)
+        } else if (i == -1) {
+            doing.append(Day(key: dateKey, date: stringDate))
+            doing[doing.count-1].addDoing(Doing: txtDoing.text!)
         }
-        arr.append(txtDoing.text!)
         
-        ud.set(arr, forKey: "arr")
+        ud.set(try? PropertyListEncoder().encode(doing), forKey: "day")
         
-        viewWillAppear(true)
+        outputDoing()
         
     }
     
@@ -51,10 +119,12 @@ class AddDoing : UIViewController {
     }
     
     @IBAction func arrInit(_ sender: Any) {
-        if var arr = ud.stringArray(forKey: "arr") {
-            arr = []
-            ud.set(arr, forKey: "arr")
-        }
+//        if var arr = ud.stringArray(forKey: "arr") {
+//            arr = []
+//            ud.set(arr, forKey: "arr")
+//        }
+        doing = []
+        ud.set(try? PropertyListEncoder().encode(doing), forKey: "day")
     }
     
     
--- ToDoList/Day.swift
+++ ToDoList/Day.swift
@@ -7,26 +7,51 @@
 
 import Foundation
 
-class Day {
-    var date:DateFormatter
-    var doing:[Doing] = []
+class Day: Codable {
+    let key: String?
+    let date: String?
+    var doing: [String] = []
     
-    init(date: DateFormatter) {
+    init(key: String?, date: String?) {
+        self.key = key
         self.date = date
     }
     
-    func addContent(content:String) {
-        doing.append(Doing(checkBool: false, dayContent: content))
+    func addDoing(Doing:String) {
+        doing.append(Doing)
     }
+    
+    func outputDoing() -> String {
+        var List: String = ""
+        for i in 0..<doing.count {
+            List += (doing[i] + "\n")
+        }
+        return List
+    }
+    
+    
 }
 
-class Doing {
-    var checkBool:Bool = false
-    ///var everydayContent:String
-    var dayContent:String
-    
-    init(checkBool: Bool, dayContent: String) {
-        self.checkBool = checkBool
-        self.dayContent = dayContent
-    }
-}
+//class Day {
+//    var date:DateFormatter
+//    var doing:[Doing] = []
+//
+//    init(date: DateFormatter) {
+//        self.date = date
+//    }
+//
+//    func addContent(content:String) {
+//        doing.append(Doing(checkBool: false, dayContent: content))
+//    }
+//}
+//
+//class Doing {
+//    var checkBool:Bool = false
+//    ///var everydayContent:String
+//    var dayContent:String
+//
+//    init(checkBool: Bool, dayContent: String) {
+//        self.checkBool = checkBool
+//        self.dayContent = dayContent
+//    }
+//}
